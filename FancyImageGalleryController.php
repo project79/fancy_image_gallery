f8c6735e28660723db94f24993fd48458f932eb0
project79 <dejan79@gmail.com>
2010-11-29 23:47:46 +0100

    rev001
    fancy v 0.8.3 za wolf 0.7.0

---
+++ FancyImageGalleryController.php
@@ -0,0 +1,219 @@
+<?php
+
+/**
+ * Fancy Image Gallery for Wolf CMS- Create gallery and display images with few clicks
+ * Gallery is free for non-profit usage. For commercial usage, please contact one of the authors.
+ * @package wolf
+ * @subpackage plugin.fancy_image_gallery
+ *
+ * @author Sanja Andjelkovic <sanja@medio.com.hr>
+ * @author Dejan Andjelkovic <dejan@medio.com.hr>
+ * @version 0.8.3
+ * @for Wolf version 0.6.0 and above
+ * @license http://www.gnu.org/licenses/gpl.html GPL License
+ * @copyright medio.com.hr, 2009-2010
+ */
+if (!defined('IN_CMS')) { exit(); }
+class FancyImageGalleryController extends PluginController {
+
+	public static function _checkLog() {
+        AuthUser::load();
+        if ( ! AuthUser::isLoggedIn()) {
+            redirect(get_url('login'));
+        }
+    }
+	
+    public function __construct() {
+		self::_checkLog();
+		
+        $this->setLayout('backend');
+        $this->assignToLayout('sidebar', new View('../../plugins/fancy_image_gallery/views/sidebar'));
+    }
+
+    function index($page = 0)
+    {
+        $this->display('fancy_image_gallery/views/settings');
+    }
+
+    public function documentation() {
+        $this->display('fancy_image_gallery/views/documentation');
+    }
+
+    function settings() {
+
+        $this->display('fancy_image_gallery/views/settings', Plugin::getAllSettings('fancy_image_gallery'));
+    }
+
+public function create_thumb(){
+
+            $path = $_POST['gallery_path'];
+            
+            if(substr($path,-1) != '/')
+            {
+                Flash::set('error', __('Put the trailing slash at the end of folder name!'));
+                redirect(get_url('plugin/fancy_image_gallery/'));
+            }
+
+            $fullpath = str_replace ('?', '',BASE_URL) . 'public/images/' . $path;
+
+            $image_dir = CMS_ROOT . '/public/images/' . $path;
+
+            if(! is_dir($image_dir))
+            {
+                Flash::set('error', __('The folder does not exist!'));
+                redirect(get_url('plugin/fancy_image_gallery/'));
+            }
+            
+            $handle = opendir($image_dir);
+            $count_images = 0;
+
+            if ($handle)
+            {
+                while (false !== ($file = readdir($handle)))
+		{
+			if ($file != '.' && $file != '..')
+			{
+                           
+                if(strstr($file,'.jpg') || strstr($file,'.JPG') || strstr($file,'.png') || strstr($file,'.PNG') || strstr($file,'.gif') || strstr($file,'.GIF'))
+				{
+
+                                        if(strtolower(substr($file,-10,-4)) == "-thumb")
+                                        {
+                                            unlink($image_dir . '/' . $file);
+                                        }
+                                        else{
+                                        $files[] = $file;
+                                        }
+				
+                                        $count_images++;
+                                }
+                           
+			}
+		}
+		closedir($handle);
+            }
+
+            $counter = 0;
+            $thumb_width = $_POST['thumb_width'];
+            $thumb_height = $_POST['thumb_height'];
+
+            if($thumb_width == NULL)
+            {
+                Flash::set('error', __('Specify thumbnail width!'));
+                redirect(get_url('plugin/fancy_image_gallery/'));
+            }
+
+            if($thumb_height == NULL)
+            {
+                Flash::set('error', __('Specify thumbnail height!'));
+                redirect(get_url('plugin/fancy_image_gallery/'));
+            }
+
+            if($count_images == 0)
+            {
+                Flash::set('error', __('There are no images in this folder!'));
+                redirect(get_url('plugin/fancy_image_gallery/'));
+            }
+
+            else
+            {
+                if($files)
+			{
+                  while ($counter<=count($files)-1)
+                  {
+			foreach($files as $file)
+			{
+                        
+                            $images = $files[$counter]; 
+			
+                     
+
+                                if(substr($images,-3) == "jpg")
+                                {
+                                    $starting_image = imagecreatefromjpeg($image_dir . $images);
+                                    $width = imagesx($starting_image);
+                                    $height = imagesy($starting_image);
+
+
+                                    $images = str_replace ('.jpg', '', $images);
+                                    //$images = str_replace ('.JPG', '', $images);
+
+                                    $thumb_image = imagecreatetruecolor($thumb_width, $thumb_height);
+                                    imagecopyresampled($thumb_image, $starting_image, 0, 0, 0, 0, $thumb_width, $thumb_height, $width, $height);
+                                    imagejpeg($thumb_image, $image_dir . $images . '-thumb.jpg');
+                                
+                                }
+								elseif(substr($images,-3) == "JPG")
+                                {
+                                    $starting_image = imagecreatefromjpeg($image_dir . $images);
+                                    $width = imagesx($starting_image);
+                                    $height = imagesy($starting_image);
+
+                                    $images = str_replace ('.JPG', '', $images);
+
+                                    $thumb_image = imagecreatetruecolor($thumb_width, $thumb_height);
+                                    imagecopyresampled($thumb_image, $starting_image, 0, 0, 0, 0, $thumb_width, $thumb_height, $width, $height);
+                                    imagejpeg($thumb_image, $image_dir . $images . '-thumb.JPG');
+                                
+                                }
+
+                                elseif(substr($images,-3) == "gif")
+                                {
+                                    $starting_image = imagecreatefromgif($image_dir . $images);
+                                    $width = imagesx($starting_image);
+                                    $height = imagesy($starting_image);
+
+                                    $images = str_replace ('.gif', '', $images);
+
+                                    $thumb_image = imagecreatetruecolor($thumb_width, $thumb_height);
+                                    imagecopyresampled($thumb_image, $starting_image, 0, 0, 0, 0, $thumb_width, $thumb_height, $width, $height);
+                                    imagegif($thumb_image, $image_dir . $images . '-thumb.gif');
+                                }
+								elseif(substr($images,-3) == "GIF")
+                                {
+                                    $starting_image = imagecreatefromgif($image_dir . $images);
+                                    $width = imagesx($starting_image);
+                                    $height = imagesy($starting_image);
+
+                                    $images = str_replace ('.GIF', '', $images);
+
+                                    $thumb_image = imagecreatetruecolor($thumb_width, $thumb_height);
+                                    imagecopyresampled($thumb_image, $starting_image, 0, 0, 0, 0, $thumb_width, $thumb_height, $width, $height);
+                                    imagejpeg($thumb_image, $image_dir . $images . '-thumb.GIF');
+                                
+                                }
+				    			elseif(substr($images,-3) == "PNG")
+                                {
+                                    $starting_image = imagecreatefrompng($image_dir . $images);
+                                    $width = imagesx($starting_image);
+                                    $height = imagesy($starting_image);
+
+                                    $images = str_replace ('.PNG', '', $images);
+
+                                    $thumb_image = imagecreatetruecolor($thumb_width, $thumb_height);
+                                    imagecopyresampled($thumb_image, $starting_image, 0, 0, 0, 0, $thumb_width, $thumb_height, $width, $height);
+                                    imagegif($thumb_image, $image_dir . $images . '-thumb.PNG');
+                                }
+
+                                else
+                                {
+                                    $starting_image = imagecreatefrompng($image_dir . $images);
+                                    $width = imagesx($starting_image);
+                                    $height = imagesy($starting_image);
+
+                                    $images = str_replace ('.png', '', $images);
+
+                                    $thumb_image = imagecreatetruecolor($thumb_width, $thumb_height);
+                                    imagecopyresampled($thumb_image, $starting_image, 0, 0, 0, 0, $thumb_width, $thumb_height, $width, $height);
+                                    imagepng($thumb_image, $image_dir . $images . '-thumb.png');
+                                }
+                                $counter++;
+                         }
+
+                    }
+                    Flash::set('success', __('Thumbnails are successfully created!'));
+                    redirect(get_url('plugin/fancy_image_gallery/'));
+                }
+            }
+	}
+}
---
+++ index.php
@@ -0,0 +1,134 @@
+<?php
+
+/**
+ * Fancy Image Gallery for Wolf CMS- Create gallery and display images with few clicks
+ * Gallery is free for non-profit usage. For commercial usage, please contact one of the authors.
+ * @package wolf
+ * @subpackage plugin.fancy_image_gallery
+ *
+ * @author Sanja Andjelkovic <sanja@medio.com.hr>
+ * @author Dejan Andjelkovic <dejan@medio.com.hr>
+ * @version 0.8.3
+ * @for Wolf version 0.6.0 and above
+ * @license http://www.gnu.org/licenses/gpl.html GPL License
+ * @copyright medio.com.hr, 2009-2010
+ */
+ 
+Plugin::setInfos(array(
+    'id'          => 'fancy_image_gallery',
+    'title'       => 'Fancy Image Gallery',
+    'description' => __('Provides easy to use image gallery with fancybox effect.'),
+    'version'     => '0.8.3',
+	'license'     => 'GPL',
+	'author'      => 'Sanja Andjelkovic',	
+    'website'     => 'http://medio.com.hr/',
+	'update_url'  => 'http://www.project79.net/plugin-versions.xml',
+	'require_wolf_version' => '0.6.0'
+));
+
+Plugin::addController('fancy_image_gallery', 'Fancy Image Gallery');
+
+// funkcija poziva css i fancybox iz foldera /js i /resources
+function fancy_resources(){
+	//putanje
+	$jspath = str_replace ('?', '',BASE_URL).'wolf/plugins/fancy_image_gallery/js/';
+	$csspath = str_replace ('?', '',BASE_URL).'wolf/plugins/fancy_image_gallery/resources/';
+		
+	// loadaj fancybox i pripadajuce css fajlove
+	echo '<script type="text/javascript" src="',$jspath,'jquery.fancybox-1.3.0.pack.js"></script>',"\n";
+	echo '<link href="',$csspath,'jquery.fancybox-1.3.0.css" rel="stylesheet" type="text/css">',"\n";
+}
+
+
+// funkcija koja izbacuje samo jednu sliku iz direktorija i pravi link na odabranu galeriju
+function fancy_parent($path, $child){
+
+        $fullpath = str_replace ('?', '',BASE_URL) . 'public/images/' . $path;
+
+        $image_dir = CMS_ROOT . '/public/images/' . $path;
+                
+		$handle = opendir($image_dir);
+
+                
+		if ($handle) {
+			while (false !== ($file = readdir($handle)))
+			{
+				if ($file != '.' && $file != '..')
+				{
+					if(strstr($file,'-thumb'))
+					{
+						$files[] = $file;
+                                                
+					}
+				}
+			}
+			closedir($handle);
+		}
+
+		// propusti kroz petlju i ispisi linkove, te ih vezi za galeriju
+		// za title ispisi samo krajnji direktorij u kojem se nalaze slike
+                
+		$images = $files[0];
+        $path = str_replace(dirname($path), '', $path);
+
+                if($files)
+		{
+
+                    echo '<a class="link" rel="show-me-all" href="',BASE_URL . $child,'" title="',str_replace('/','',$path),'"><img src="',$fullpath,$images,'" width="125" height="100" /></a>',"\n";
+
+                    
+                }
+		else
+		{
+			echo __('There are no images in this gallery.');
+		}
+}
+
+function fancy($path){
+         
+        $fullpath = str_replace ('?', '',BASE_URL) . 'public/images/' . $path;
+
+        $image_dir = CMS_ROOT . '/public/images/' . $path;
+	        
+		$handle = opendir($image_dir);
+		
+			
+		// prvi korak:  citaj direktorij i napravi polje
+		if ($handle) {
+			while (false !== ($file = readdir($handle)))
+			{
+				if ($file != '.' && $file != '..') 
+				{
+					if(strstr($file,'-thumb'))
+					{
+						$files[] = $file;
+					}
+				}
+			}
+			closedir($handle);
+		}
+		
+		// drugi korak - propusti kroz petlju i ispisi slike
+		$images = count($files);
+                $counter=1;
+
+
+                if(count($files))
+		{
+				  natsort($files); //sortiranje; dodano u 0.8.1
+                  while ($counter<=$images)
+                  {
+			foreach($files as $file)
+			{
+				$counter++;
+				echo '<a class="photo" rel="my-gallery" href="',$fullpath,str_replace('-thumb','',$file),'" title="',str_replace('-thumb','',$file),'"><img src="',$fullpath,$file,'"/></a>';
+			}
+                   
+                 }
+        }
+		else
+		{
+			echo __('There are no images in this gallery.');
+		}
+}
+?>
