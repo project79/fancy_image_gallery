b11be1fb1fa6f7697eb42560b4baabda206b53c7
project79 <dejan79@gmail.com>
2010-12-04 22:57:36 +0100

    rev002
    Russian translation Added

---
+++ i18n/ru-message.php
@@ -0,0 +1,34 @@
+<?php
+
+    /**
+     * Russian language file for gallery plugin
+     *
+     * @package wolf
+     * @subpackage plugin.fancy_image_gallery.translations
+     *
+     * @author Konstantin Baev <konstantin.baev@gmail.com>
+     * @version Wolf 0.6.0 + above
+     */
+
+    return array(
+    'Documentation' => 'Документация',
+	'Settings' => 'Настройки',
+	'Create Thumbnails - All fields are mandatory' => 'Создать миниатюры - все поля обязательны для заполнения',
+	'Gallery Path' => 'Путь к галерее',
+	'Specify name of the folder under "public/images/" with a trailing slash. Example: mygallery/' =>
+	'Укажите имя папки, расположенной в "public/images/" вместе со слешом в конце. Пример: gallery/',
+	'Thumbnail Width' => 'Ширина миниатюры',
+	'Specify thumbnail width in pixels' => 'Укажите ширину миниатюры в пикселях',
+	'Thumbnail Height' => 'Высота миниатюры',
+	'Specify thumbnail height in pixels' => 'Укажите высоту миниатюры в пикселях',
+	'Create Thumbnails' => 'Создать миниатюры',
+	'Thumbnails are successfully created!' => 'Миниатюры успешно созданы',
+	'There are no images in this folder!' => 'Папка не содержит изображений!',
+	'Provides easy to use image gallery with fancybox effect.' =>
+	'Предоставляет простую в использовании галерею изображений с fancybox-эффектом.',
+	'Put the trailing slash at the end of the folder name!' => 'Необходимо поставить слеш (/) в конце имени папки!',
+	'The folder does not exist!' => 'Папка не существует!',
+	'Specify thumbnail width!' => 'Укажите ширину миниатюры!',
+	'Specify thumbnail height!' => 'Укажите высоту миниатюры!',
+	'There are no images in this gallery.' => 'В галерее отсутствуют изображения.'
+    );
