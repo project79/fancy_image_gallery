832b58c55afe30d8c76839ef02e3b181e25889ea
project79 <dejan79@gmail.com>
2013-04-23 08:52:04 +0200

    English translation
    Added for 0.7.7

---
+++ i18n/en-message.php
@@ -0,0 +1,32 @@
+<?php
+
+    /**
+     * English language file for gallery plugin
+     *
+     * @package wolf
+     * @subpackage plugin.fancy_image_gallery.translations
+     *
+     * @author Dejan Andjelkovic <dejan@medio.com.hr>
+     * @version Wolf 0.7.7 + above
+     */
+
+    return array(
+    'Documentation' => 'Documentation',
+	'Settings' => 'Settings',
+	'Create Thumbnails - All fields are mandatory' => 'Create Thumbnails - All fields are mandatory',
+	'Gallery Path' => 'Gallery Path',
+	'Specify name of the folder under "public/images/" with a trailing slash. Example: mygallery/' => 'Specify name of the folder under "public/images/" with a trailing slash. Example: mygallery/',
+	'Thumbnail Width' => 'Thumbnail Width',
+	'Specify thumbnail width in pixels' => 'Specify thumbnail width in pixels',
+	'Thumbnail Height' => 'Thumbnail Height',
+	'Specify thumbnail height in pixels' => 'Specify thumbnail height in pixels',
+	'Create Thumbnails' => 'Create Thumbnails',
+	'Thumbnails are successfully created!' => 'Thumbnails are successfully created!',
+	'There are no images in this folder!' => 'There are no images in this folder!',
+	'Provides easy to use image gallery with fancybox effect.' => 'Provides easy to use image gallery with fancybox effect.',
+	'Put the trailing slash at the end of the folder name!' => 'Put the trailing slash at the end of the folder name!',
+	'The folder does not exist!' => 'The folder does not exist!',
+	'Specify thumbnail width!' => 'Specify thumbnail width!',
+	'Specify thumbnail height!' => 'Specify thumbnail height!',
+	'There are no images in this gallery.' => 'There are no images in this gallery.'
+    );
